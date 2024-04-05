08e254efdb1b184be7531a00a6c85a072500cde4
Faure <faurev@3il.fr>
2024-04-05 13:59:53 +0200

    Ajout de l'entite Tache et de son Enum Priorite

---
+++ src/model/Priorite.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ * Enumération représentant le type de priorite d'une tache
+ */
+public enum Priorite {
+	
+	HAUTE,
+	MOYENNE,
+	BASSE;
+
+}
---
+++ src/model/Tache.java
@@ -0,0 +1,59 @@
+package model;
+
+/**
+ * Une tache est représenter par un titre, une description, et un niveau de priorité
+ */
+public class Tache {
+
+	/**
+	 * Niveau de priorité de la tache
+	 */
+	private Priorite priorite;
+	
+	/**
+	 * Titre de la tache
+	 */
+	private String titre;
+	
+	/**
+	 * Description de la tache
+	 */
+	private String description;
+
+	/**
+	 * Construction d'une tache
+	 * @param priorite Niveau de priorite
+	 * @param titre Titre de la tache
+	 * @param description Description de la tache
+	 */
+	public Tache(Priorite priorite, String titre, String description) {
+		super();
+		this.priorite = priorite;
+		this.titre = titre;
+		this.description = description;
+	}
+
+	public Priorite getPriorite() {
+		return priorite;
+	}
+
+	public void setPriorite(Priorite priorite) {
+		this.priorite = priorite;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
